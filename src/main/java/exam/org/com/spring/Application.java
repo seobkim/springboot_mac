0a542b9c12d59621383102df92be927ea0596ac1
seobkim <kks7824@gmail.com>
2021-02-12 15:55:31 +0900

    2.12 term commit

---
+++ src/main/java/exam/org/com/spring/Application.java
@@ -0,0 +1,15 @@
+package exam.org.com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[]args){
+        SpringApplication.run(Application.class,args);
+
+    }
+
+
+}
